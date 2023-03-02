780b4d1b547b973bff826bb8e66c8510b4149b90
OliverAndreasen <oliv2058@stud.kea.dk>
2023-03-02 22:11:14 +0100

    removed stuff

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-
 // the express.static() method is used to serve static files such as images, CSS files, and JavaScript files.
 // because of security reasons, you can only access files that are in the public folder
 app.use('/public', express.static(__dirname + '/public' ));
